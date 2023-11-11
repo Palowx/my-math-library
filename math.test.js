a677362573a083654e97610246db8614e736efb4
Ploy <p.prakaiploy@gmail.com>
2023-11-11 11:06:31 +0700

    Add sum arr fn

--- math.test.js
+++ math.test.js
@@ -2,3 +2,4 @@ const math = require('./math')
 const assert = require('assert')
 
 assert.strictEqual(math.sum(2,2),4,'should sum two nums')
+assert.strictEqual(math.sum([1,2,3,4,6]),16,'should sum array')
